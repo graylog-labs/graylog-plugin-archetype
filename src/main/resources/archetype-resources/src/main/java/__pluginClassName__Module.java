c2bed9e905f3b0a296eedd6e69e022576ac7377b
Jochen Schalanda <jochen@torch.sh>
2015-01-12 12:42:42 +0100

    Update PluginModule template

--- src/main/resources/archetype-resources/src/main/java/__pluginClassName__Module.java
+++ src/main/resources/archetype-resources/src/main/java/__pluginClassName__Module.java
@@ -1,14 +1,29 @@
 package ${package};
 
+import org.graylog2.plugin.PluginConfigBean;
 import org.graylog2.plugin.PluginModule;
+
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * Extend the PluginModule abstract class here to add you plugin to the system.
  */
 public class ${pluginClassName}Module extends PluginModule {
+    /**
+     * Returns all configuration beans required by this plugin.
+     *
+     * Implementing this method is optional. The default method returns an empty {@link Set}.
+     */
+    @Override
+    public Set<? extends PluginConfigBean> getConfigBeans() {
+        return Collections.emptySet();
+    }
+
     @Override
     protected void configure() {
-        /* Register your plugin types here.
+        /*
+         * Register your plugin types here.
          *
          * Examples:
          *
@@ -18,6 +33,12 @@ public class ${pluginClassName}Module extends PluginModule {
          * addPeriodical(Class<? extends Periodical>);
          * addAlarmCallback(Class<? extends AlarmCallback>);
          * addInitializer(Class<? extends Service>);
+         * addRestResource(Class<? extends PluginRestResource>);
+         *
+         *
+         * Add all configuration beans returned by getConfigBeans():
+         *
+         * addConfigBeans();
          */
     }
 }
