4fb17d3fe32576811c5cc7b9d7ded0d1ec5c45a6
Edmundo Alvarez <edmundo@graylog.com>
2016-04-26 15:44:02 +0200

    Use 2.0.0 as required version in metadata

--- src/main/resources/archetype-resources/src/main/java/__pluginClassName__MetaData.java
+++ src/main/resources/archetype-resources/src/main/java/__pluginClassName__MetaData.java
@@ -47,7 +47,7 @@ public class ${pluginClassName}MetaData implements PluginMetaData {
 
     @Override
     public Version getRequiredVersion() {
-        return new Version(1, 2, 0);
+        return new Version(2, 0, 0);
     }
 
     @Override
